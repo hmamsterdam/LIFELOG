645669622fd68256de88824785b843d0538576df
hmamsterdam <35189225+hmamsterdam@users.noreply.github.com>
2019-02-19 20:45:50 +0900

    Add files via upload

---
+++ component_menubar.blade.php
@@ -0,0 +1,9 @@
+<style>
+li { display:inline-block; margin:0px 40px; font-size:30pt; }
+a { text-decoration:none; color:#cccccc; }
+a:hover { color:#ffff00; }
+</style>
+<li>{{$menu1}}</li>
+<li><a href="{{$menu2}}.php">{{$menu2}}</a></li>
+<li><a href="{{$menu3}}.php">{{$menu3}}</a></li>
+<li><a href="{{$menu4}}.php">{{$menu4}}</a></li>
